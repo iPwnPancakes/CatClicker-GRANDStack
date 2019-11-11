993f8ca7b99e2e88b1b633bf183dfb1e77ce753d
Daniel Buenrrostro <Daniel@DanielBuenrrostro.com>
2019-11-11 15:38:38 -0600

    Renamed variable in getParents query

--- backend/index.js
+++ backend/index.js
@@ -2,28 +2,28 @@ require('dotenv').config();
 const neo4j = require('neo4j-driver').v1;
 const express = require('express');
 const cors = require('cors');
-const {ApolloServer} = require('apollo-server-express');
-const {makeAugmentedSchema} = require('neo4j-graphql-js');
+const { ApolloServer } = require('apollo-server-express');
+const { makeAugmentedSchema } = require('neo4j-graphql-js');
 const typeDefs = require('./src/TypeDefs');
 const CreateResolvers = require('./src/Resolvers');
 
-const driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD), {disableLosslessIntegers: true});
+const driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD), { disableLosslessIntegers: true });
 const session = driver.session();
 const apolloServer = new ApolloServer({
     schema: makeAugmentedSchema({
         typeDefs,
         resolvers: CreateResolvers(session)
     }),
-    context: {driver}
+    context: { driver }
 });
 
 const app = express();
 const ENV = process.env.ENVIRONMENT || 'PRODUCTION';
 const PORT = process.env.PORT || 8080;
 
-apolloServer.applyMiddleware({app});
+apolloServer.applyMiddleware({ app });
 
-if (ENV !== 'PRODUCTION') {
+if (ENV !== 'PRODUCTION' || ENV !== 'PROD') {
     console.warn('CORS is enabled, make sure this isnt on a production site!');
     app.use(cors());
 }
@@ -33,6 +33,6 @@ app.get('/', (request, response) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    console.log(`GraphQL Endpoint ${apolloServer.graphqlPath}`);
+    console.log(`Listening on port ${ PORT }`);
+    console.log(`GraphQL Endpoint ${ apolloServer.graphqlPath }`);
 });
